513a31d910eabceb2779ebad8dab5e4c93833a3b
Sasha Klizhentas <klizhentas@gmail.com>
2016-11-29 11:42:12 -0800

    add more comments

--- images/hook/entrypoint.sh
+++ images/hook/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/dumb-init /bin/sh
 set -e
 
+echo "Asuming changeset from the envrionment: $RIG_CHANGESET"
+# note that rig does not take explicit changeset ID
+# taking it from the environment variables
 if [ $1 = "update" ]; then
     echo "Starting update, changeset: $RIG_CHANGESET"
     echo "Deleting old replication controller rc/log-forwarder"
@@ -11,7 +14,9 @@ if [ $1 = "update" ]; then
     rig status $RIG_CHANGESET --retry-attempts=120 --retry-period=1s --debug
     echo "Freezing"
     rig freeze
-else
+elif [ $1 = "rollback" ]; then
     echo "Rolling back, changeset: $RIG_CHANGESET"
     rig rollback
+else
+    echo "Missing argument, should be either 'update' or 'rollback'"
 fi
