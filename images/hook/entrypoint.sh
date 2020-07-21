a5b0b9d548f54dad77e58f5f2947215b378d2d47
Emanuele 'Lele' Calo <emanuele.lele.calo@gmail.com>
2020-07-21 20:45:47 +0200

    Fixed log-forwarder update issue

--- images/hook/entrypoint.sh
+++ images/hook/entrypoint.sh
@@ -5,7 +5,24 @@ set -e
 # taking it from the environment variables
 
 echo "--> Assuming changeset from the environment: $RIG_CHANGESET"
-if [ $1 = "update" ]; then
+if [ $1 = "install" ]; then
+    echo "Checking: $RIG_CHANGESET"
+    if rig status $RIG_CHANGESET --retry-attempts=1 --retry-period=1s; then exit 0; fi
+
+    echo "Starting installation, changeset: $RIG_CHANGESET"
+    # deleting in case it has been already attempted
+    rig cs delete --force -c cs/$RIG_CHANGESET
+    echo "Creating Log Forwarder ConfigMap"
+    rig upsert -f /var/lib/gravity/resources/init_logforwarder.yaml --debug
+    echo "--> Creating new resources"
+    for file in /var/lib/gravity/resources/app/*.yaml; do
+        rig upsert -f $file --debug
+    done
+    echo "Checking status"
+    rig status $RIG_CHANGESET --retry-attempts=120 --retry-period=1s --debug
+    echo "Freezing"
+    rig freeze
+elif [ $1 = "update" ]; then
     echo "--> Checking: $RIG_CHANGESET"
     if rig status ${RIG_CHANGESET} --retry-attempts=1 --retry-period=1s; then exit 0; fi
 
