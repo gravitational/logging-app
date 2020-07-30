decd6daccffc1b083ab0a81e7c29b8bc74954434
Emanuele 'Lele' Calo <emanuele.lele.calo@gmail.com>
2020-07-30 17:11:59 +0200

    Fixed ConfigMap name

--- images/hook/entrypoint.sh
+++ images/hook/entrypoint.sh
@@ -13,7 +13,7 @@ if [ $1 = "install" ]; then
     # deleting in case it has been already attempted
     rig cs delete --force -c cs/$RIG_CHANGESET
 
-    if ! kubectl get configmap name >/dev/null 2>&1
+    if ! kubectl get configmap log-forwarders >/dev/null 2>&1
     then
         echo "Creating Log Forwarder ConfigMap"
         rig upsert -f /var/lib/gravity/resources/logforwarder.yaml --debug
